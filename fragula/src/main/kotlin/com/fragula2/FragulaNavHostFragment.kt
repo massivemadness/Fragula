c9b301cfd06bd94d8c767587fc197b364da44ec5
Dmitry Rubtsov <dm.mironov01@gmail.com>
2022-03-07 22:17:04 +0300

    Get rid of reflection API

--- fragula/src/main/kotlin/com/fragula2/FragulaNavHostFragment.kt
+++ fragula/src/main/kotlin/com/fragula2/FragulaNavHostFragment.kt
@@ -1,17 +1,15 @@
 package com.fragula2
 
+import android.view.View
 import androidx.navigation.NavHostController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.plusAssign
+import com.blacksquircle.fragula.R
 
 class FragulaNavHostFragment : NavHostFragment() {
 
     private val navContainerId: Int
-        get() {
-            val method = javaClass.superclass.getDeclaredMethod("getContainerId")
-            method.isAccessible = true
-            return method.invoke(this) as Int
-        }
+        get() = if (id != 0 && id != View.NO_ID) id else R.id.nav_host_fragment_container
 
     override fun onCreateNavHostController(navHostController: NavHostController) {
         super.onCreateNavHostController(navHostController)
