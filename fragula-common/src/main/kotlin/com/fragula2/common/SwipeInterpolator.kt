db3c74017c7ef6dba68ba7f25dec920c675dbafe
Dmitrii Rubtsov <dm.mironov01@gmail.com>
2022-12-17 10:43:05 +0200

    Update AGP to 7.3.1 and SDK to 33
    + Update libraries

--- fragula-common/src/main/kotlin/com/fragula2/common/SwipeInterpolator.kt
+++ fragula-common/src/main/kotlin/com/fragula2/common/SwipeInterpolator.kt
@@ -3,5 +3,5 @@ package com.fragula2.common
 import android.view.animation.DecelerateInterpolator
 
 // Same interpolator used in Telegram X
-// https://github.com/TGX-Android/X-Android/blob/main/src/main/java/me/vkryl/android/AnimatorUtils.java#L37
+// https://github.com/TGX-Android/X-Android/blob/main/src/main/java/me/vkryl/android/AnimatorUtils.java#L38
 class SwipeInterpolator : DecelerateInterpolator(1.78f)
