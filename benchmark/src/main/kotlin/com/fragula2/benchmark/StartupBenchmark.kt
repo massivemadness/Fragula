218459d658fa57b52c91f8c77298a5ff63723dde
Dmitrii Rubtsov <dm.mironov01@gmail.com>
2023-01-28 08:46:34 +0200

    Baseline Profile
    + Bump minSdk to 23

---
+++ benchmark/src/main/kotlin/com/fragula2/benchmark/StartupBenchmark.kt
@@ -0,0 +1,34 @@
+package com.fragula2.benchmark
+
+import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class StartupBenchmark {
+
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
+
+    @Test
+    fun startupCompilationNone() = startup(CompilationMode.None())
+
+    @Test
+    fun startupCompilationPartial() = startup(CompilationMode.Partial())
+
+    private fun startup(compilationMode: CompilationMode) = benchmarkRule.measureRepeated(
+        packageName = "com.fragula2.sample",
+        metrics = listOf(StartupTimingMetric()),
+        compilationMode = compilationMode,
+        startupMode = StartupMode.COLD,
+        iterations = 5,
+    ) {
+        pressHome()
+        startActivityAndWait()
+    }
+}
