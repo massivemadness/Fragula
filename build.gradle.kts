b14858db8f5ae75c2c9c742a8e0fb32acb25a456
vicky leu <yu6564172@gmail.com>
2023-05-04 19:01:28 +0800

    remove unused code

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.0.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
     }
 }
 
--- fragula-compose/build.gradle.kts
+++ fragula-compose/build.gradle.kts
@@ -2,11 +2,9 @@
 plugins {
     id("com.android.library")
     kotlin("android")
+    `maven-publish`
 }
 
-
-group = "com.fragula2.compose"
-version = "2.7.0"
 
 
 android {
@@ -38,6 +36,37 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+//    publishing {
+//        publications {
+//            create<MavenPublication>("maven") {
+//                groupId = "com.fragula2.compose"
+//                artifactId = "compose"
+//                version = "2.7.0"
+//                from(components["kotlin"])
+//            }
+//        }
+//    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            register("release", MavenPublication::class) {
+                // Applies the component for the release build variant.
+                // NOTE : Delete this line code if you publish Native Java / Kotlin Library
+                from(components["release"])
+                // Library Package Name (Example : "com.frogobox.androidfirstlib")
+                // NOTE : Different GroupId For Each Library / Module, So That Each Library Is Not Overwritten
+                groupId = "com.fragula2"
+                // Library Name / Module Name (Example : "androidfirstlib")
+                // NOTE : Different ArtifactId For Each Library / Module, So That Each Library Is Not Overwritten
+                artifactId = "compose"
+                // Version Library Name (Example : "1.0.0")
+                version = "2.7.0"
+            }
+        }
     }
 }
 
